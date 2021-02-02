4212c6431ff76cd3b637b03cccf2ef0083461de1
Afroza Akter Ruma <afroza.akter.ruma035@gmail.com>
2021-02-02 08:45:42 +0600

    truthy vs falsy

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//falsy
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+//truthy
+//'0' '' []
+// const age = 5;
+let name = null;
+console.log(name);
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}

5c3b13e08069456a78b271c9003356a40c8ea50b
Afroza Akter Ruma <afroza.akter.ruma035@gmail.com>
2021-02-02 21:24:18 +0600

    break-continue

---
+++ scope.js
@@ -0,0 +1,17 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+       var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky";
+        // console.log(mood);
+    }
+}
+
+var day = "friday";
+console.log(day);
+return result;

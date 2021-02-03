1bb72fdfefef72bc5643d0c79aa370ee9fe681c6
Afroza Akter Ruma <afroza.akter.ruma035@gmail.com>
2021-02-03 08:52:48 +0600

    scope-khoja

--- scope.js
+++ scope.js
@@ -2,13 +2,13 @@ let bonus = 20;
 
 function sum(first, second){
     let result = first + second + bonus;
-    // console.log(bonus);
+    console.log(bonus);
     if(result > 9){
        var mood = "happy";
         mood = "fishy";
         mood = "funky";
         mood = "cranky";
-        // console.log(mood);
+        console.log(mood);
     }
 }
 
